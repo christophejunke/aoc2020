ca164bf9c157b3854d58790721cfd49cbb0af27a
Christophe Junke <junke.christophe@gmail.com>
2020-11-29 12:28:03 +0100

    Fix assert (facepalm)

--- d00.lisp
+++ d00.lisp
@@ -1,4 +1,5 @@
 (defpackage :aoc2020.00 (:use :aoc2020))
 (in-package :aoc2020.00)
 
-(assert (do-input-lines (line 0 t) (string= line "test")))
+(do-input-lines (line 0)
+  (assert (string= line "test")))
