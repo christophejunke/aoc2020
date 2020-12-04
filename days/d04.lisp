1e1203f706836a369c2000a8cdc7c2292a4c4b6f
Christophe Junke <junke.christophe@gmail.com>
2020-12-04 09:14:50 +0100

    Day 04 - part 1

--- days/d04.lisp
+++ days/d04.lisp
@@ -3,3 +3,44 @@
 
 (in-package :aoc2020.04)
 
+(defun map-chunks (fn &aux stack)
+  (flet ((emit ()
+           (when stack
+             (let ((lines (nreverse (shiftf stack nil))))
+               (funcall fn (format nil "~{~a~^ ~}" lines))))))
+    (do-input-lines (line 4 (emit))
+      (if (string= line "")
+          (emit)
+          (push line stack)))))
+
+(defpackage aoc2020.04.fields
+  (:use)
+  (:export #:byr
+           #:iyr
+           #:eyr
+           #:hgt
+           #:hcl
+           #:ecl
+           #:pid
+           #:cid))
+
+(defun map-records (function)
+  (map-chunks
+   (lambda (line)
+     (funcall 
+      function
+      (loop
+         :for field :in (split #\space line :sharedp t)
+         :for (name value) := (split #\: field :sharedp t)
+         :collect (cons (find-symbol (string-upcase name) 
+                                     :aoc2020.04.fields)
+                        value))))))
+
+
+(defun part-1 (&aux (counter 0))
+  (map-records (lambda (record)
+                 (when (case (length record)
+                         (8 t)
+                         (7 (not (assoc 'aoc2020.04.fields:cid record))))
+                   (incf counter))))
+  counter)
