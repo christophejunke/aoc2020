37e7ce1b3460fbc38c8e5545c63e46cd3734cfcb
Christophe Junke <junke.christophe@gmail.com>
2020-12-04 11:34:29 +0100

    Refactoring, commenting, use a hash-table for validators
    Part 2 with CASE expression
    
    Evaluation took:
      0.014 seconds of real time
      0.013400 seconds of total run time (0.013400 user, 0.000000 system)
      92.86% CPU
      43,128,801 processor cycles
      1,506,144 bytes consed
    
    Part 2 with hash-table of validators
    
    Evaluation took:
      0.005 seconds of real time
      0.004680 seconds of total run time (0.004680 user, 0.000000 system)
      100.00% CPU
      15,399,341 processor cycles
      1,506,192 bytes consed

--- days/d04.lisp
+++ days/d04.lisp
@@ -6,17 +6,19 @@
 
 (in-package :aoc2020.04)
 
-(defun map-chunks (fn &aux stack)
+(defun map-line-chunks (function &aux stack)
+  "Read consecutive non-empty lines and call FUNCTION on their concatenation."
   (flet ((emit ()
            (when stack
              (let ((lines (nreverse (shiftf stack nil))))
-               (funcall fn (format nil "~{~a~^ ~}" lines))))))
+               (funcall function (format nil "~{~a~^ ~}" lines))))))
     (do-input-lines (line 4 (emit))
       (if (string= line "")
           (emit)
           (push line stack)))))
 
 (defpackage aoc2020.04.fields
+  (:documentation "Expected symbols for fields in a record.")
   (:use)
   (:export #:byr
            #:iyr
@@ -27,32 +29,39 @@
            #:pid
            #:cid))
 
-(defun map-records (fn)
-  (map-chunks
+(defun field (name)
+  (or (find-symbol (string-upcase name) :aoc2020.04.fields)
+      (error "Unexpected field ~s" name)))
+
+(defun map-records (function)
+  "Call FUNCTION with a (FIELD . VALUE) association list for all records."
+  (map-line-chunks
    (lambda (line)
-     (funcall fn
+     (funcall function
               (loop
                 :for field :in (split #\space line :sharedp t)
                 :for (name value) := (split #\: field :sharedp t)
-                :collect (cons (find-symbol (string-upcase name)
-                                            :aoc2020.04.fields)
-                               value))))))
+                :collect (cons (field name) value))))))
 
 (defun count-records-if (test &aux (counter 0))
-  (map-records (lambda (record)
-                 (when (funcall test record)
-                   (incf counter))))
+  "Count records that match TEST"
+  (map-records (lambda (u) (when (funcall test u) (incf counter))))
   counter)
 
 (defun validate-all-fields-if (test record)
+  "Check that for each expected FIELD, (FUNCALL TEST FIELD VALUE) is true.
+   VALUE is the associated value for FIELD in RECORD, and may be NIL."
   (do-external-symbols (field :aoc2020.04.fields t)
     (unless (funcall test field (cdr (assoc field record)))
       (return nil))))
 
 (defun solve-part (validation-test)
+  "Count all records that match VALIDATION-TEST"
   (count-records-if
    (lambda (record)
      (validate-all-fields-if validation-test record))))
+
+;; PART-1
 
 (defun part-1 ()
   (solve-part
@@ -63,12 +72,16 @@
        ;; other fields are mandatory
        (t value)))))
 
-(defun yearp (string min max)
+;; PART-2
+
+(declaim (inline year<= valid-height-p))
+
+(defun year<= (min string max)
   (and string
        (= 4 (length string))
        (<= min (parse-integer string) max)))
 
-(defun heightp (string)
+(defun valid-height-p (string)
   (when string
     (multiple-value-bind (height end) (parse-integer string :junk-allowed t)
       (when height
@@ -77,20 +90,48 @@
             ((unitp "in") (<=  59 height  76))
             ((unitp "cm") (<= 150 height 193))))))))
 
-(defun part-2 ()
-  (solve-part
-   (lambda (k v)
-     (macrolet ((rgxp (r) `(and v (scan ,r v)))
-                (year (min max) `(yearp v ,min ,max)))
-       (case k
-         (aoc2020.04.fields:byr (year 1920 2002))
-         (aoc2020.04.fields:iyr (year 2010 2020))
-         (aoc2020.04.fields:eyr (year 2020 2030))
-         (aoc2020.04.fields:hgt (heightp v))
-         (aoc2020.04.fields:hcl (rgxp "^#[0-9a-f]{6}$"))
-         (aoc2020.04.fields:ecl (rgxp "^(?:amb|blu|brn|gry|grn|hzl|oth)$"))
-         (aoc2020.04.fields:pid (rgxp "^\\d{9}$"))
-         (aoc2020.04.fields:cid t))))))
+(defmacro regexp (v r)
+  (check-type v symbol)
+  `(and ,v (scan ,r ,v)))
+
+;; I went into a bit of over-engineering rabbit hole
+
+(defmacro define-validators ((name var) &body clauses)
+  "Build an hash-table from fields to validation functions.
+   (check exhaustiveness, mutliple entries for a field mean AND)"
+  (check-type name symbol)
+  (check-type var symbol)
+  (with-gensyms (hash-table)
+    (let (initforms)
+      (flet ((add-initform (field expressions)
+               (push `(setf (gethash ',field ,hash-table)
+                            (compile nil
+                                     (lambda (,var)
+                                       (declare (ignorable ,var))
+                                       (and ,@expressions))))
+                     initforms)))
+        (do-external-symbols (field :aoc2020.04.fields)
+          (let ((clauses (remove field clauses :test-not #'eql :key #'first)))
+            (if clauses
+                (add-initform field (map-into clauses #'second clauses))
+                (warn "No rule for field ~s" field)))))
+      `(defvar ,name
+         (let ((,hash-table (make-hash-table)))
+           (prog1 ,hash-table
+             ,@initforms))))))
+
+(define-validators (*part-2-validators* v)
+  (aoc2020.04.fields:byr (year<= 1920 v 2002))
+  (aoc2020.04.fields:iyr (year<= 2010 v 2020))
+  (aoc2020.04.fields:eyr (year<= 2020 v 2030))
+  (aoc2020.04.fields:hgt (valid-height-p v))
+  (aoc2020.04.fields:hcl (regexp v "^#[0-9a-f]{6}$"))
+  (aoc2020.04.fields:ecl (regexp v "^(?:amb|blu|brn|gry|grn|hzl|oth)$"))
+  (aoc2020.04.fields:pid (regexp v "^\\d{9}$"))
+  (aoc2020.04.fields:cid t))
+
+(defun part-2 (&aux (h *part-2-validators*))
+  (solve-part  (lambda (k v) (funcall (gethash k h) v))))
 
 (defun test ()
   (assert (= 196 (part-1)))
