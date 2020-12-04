e97f282accafac097ba293fff16dbd874f1458e8
Christophe Junke <junke.christophe@gmail.com>
2020-12-04 09:18:26 +0100

    small refactoring

--- days/d04.lisp
+++ days/d04.lisp
@@ -27,20 +27,27 @@
 (defun map-records (function)
   (map-chunks
    (lambda (line)
-     (funcall 
+     (funcall
       function
       (loop
          :for field :in (split #\space line :sharedp t)
          :for (name value) := (split #\: field :sharedp t)
-         :collect (cons (find-symbol (string-upcase name) 
+         :collect (cons (find-symbol (string-upcase name)
                                      :aoc2020.04.fields)
                         value))))))
 
-
-(defun part-1 (&aux (counter 0))
+(defun count-records-if (test &aux (counter 0))
   (map-records (lambda (record)
-                 (when (case (length record)
-                         (8 t)
-                         (7 (not (assoc 'aoc2020.04.fields:cid record))))
+                 (when (funcall test record)
                    (incf counter))))
   counter)
+
+(defun part-1 ()
+  (count-records-if (lambda (record)
+                      (case (length record)
+                        (8 t)
+                        (7 (not
+                            (assoc 'aoc2020.04.fields:cid record)))))))
+
+(defun test ()
+  (assert (= 196 (part-1))))
