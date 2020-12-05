3adb978649057135456ec3392590d12f7458fb9e
Christophe Junke <junke.christophe@gmail.com>
2020-12-05 13:55:13 +0100

    simplified

--- days/d05.lisp
+++ days/d05.lisp
@@ -8,32 +8,28 @@
 (in-package :aoc2020.05)
 
 (defun seat-id (string)
-  (flet ((trans (c) (aref "0011" (position c "FLBR"))))
+  (flet ((trans (c) (char "0011" (position c "FLBR"))))
     (parse-integer (map-into string #'trans string) :radix 2)))
 
 (defun part-1 ()
   (with-input (i 5)
-    (loop
-      for line = (read-line i nil nil)
-      while line
-      maximize (seat-id line))))
+    (z:collect-max
+     (z:map-fn t #'seat-id (z:scan-stream i #'read-line)))))
 
 (defun part-2 ()
-  (let ((occupied (make-array (* 128 8) :element-type 'bit)))
-    (do-input-lines (line 5)
-      (setf (aref occupied (seat-id line)) 1))
-    ;; find id of unoccupied
-    (values
-     ;; find unoccupied from first occupied
-     (position 0 occupied :start (or (position 1 occupied) 0))
-     (make-array (list 128 8) :element-type 'bit
-                 :displaced-to occupied))))
+  (let ((seats (make-array (* 128 8) :element-type 'bit)))
+    (do-input-lines (line 05 (values (1+ (search #*101 seats)) seats))
+      (setf (aref seats (seat-id line)) 1))))
+
+(defun dbg/as-grid (bitvector)
+  (make-array (list 128 8) :element-type 'bit
+                           :displaced-to bitvector))
 
 (defun test-front-back-bsp ()
   (assert (= 44 (bsp (as-list "FBFBBFFRLR") 0 127))))
 
 (defun test-samples ()
-  (loop 
+  (loop
     for (s exp-id) in '(("BFFFBBFRRR" 567)
                         ("FFFBBBFRRR" 119)
                         ("BBFFBBFRLL" 820))
@@ -44,4 +40,3 @@
   (test-samples)
   (assert (= (part-1) 980))
   (assert (= (part-2) 607)))
-
