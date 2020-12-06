c950642c4f333a60d2887ec099d439603b5aa486
Christophe Junke <junke.christophe@gmail.com>
2020-12-06 09:38:39 +0100

    Day 06 - part 2

--- days/d06.lisp
+++ days/d06.lisp
@@ -7,15 +7,29 @@
 
 (in-package :aoc2020.06)
 
-(defun part-1 (&aux (counter 0))
-  (flet ((char-idx (c) (- (char-code c) #.(char-code #\a))))
-    (map-line-chunks 06 (lambda (group)
-                          (let ((mask 0))
-                            (dolist (answers group (incf counter (logcount mask)))
-                              (loop 
-                                for c across answers
-                                do (setf (logbitp (char-idx c) mask) t))))))
-    counter))
+(defun char-idx (c)
+  (- (char-code c) #.(char-code #\a)))
+
+(defun mask (answer-string)
+  (loop
+    with mask = 0
+    for c across answer-string
+    do (setf (logbitp (char-idx c) mask) t)
+    finally (return mask)))
+
+(defun solve (&aux (c1 0) (c2 0))
+  (map-line-chunks 06
+                   (lambda (group)
+                     (loop
+                       for answer-string in group
+                       for m = (mask answer-string)
+                       for p1 = m then (logior p1 m)
+                       for p2 = m then (logand p2 m)
+                       finally (incf c1 (logcount p1))
+                               (incf c2 (logcount p2)))))
+  (values c1 c2))
 
 (defun test ()
-  (assert (= (part-1) 6686)))
+  (multiple-value-bind (part-1 part-2) (solve)
+    (assert (= part-1 6686))
+    (assert (= part-2 3476))))
