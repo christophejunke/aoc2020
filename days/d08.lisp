23e427eac40bf71962ee009cd35efed6cddf54a6
Christophe Junke <junke.christophe@gmail.com>
2020-12-08 07:39:45 +0100

    Day 08 - part 2

--- days/d08.lisp
+++ days/d08.lisp
@@ -20,18 +20,34 @@
                           code
                           (array-total-size code)))))
 
+(defun run (code &aux (pc 0) (acc 0))
+  (flet ((fetch ()
+           (if (array-in-bounds-p code pc)
+               (shiftf (aref code pc) '(stop))
+               (return-from run (values acc t))))
+         (jmp (n) (incf pc n)))
+    (loop
+      (destructuring-bind (op . arg) (fetch)
+        (case op
+          (stop (return acc))
+          (nop (jmp 1))
+          (acc (incf acc arg) (jmp 1))
+          (jmp (jmp arg)))))))
+
 (defun part-1 (&optional (in 8))
-  (let ((code (program in)) (pc 0) (acc 0))
-    (flet ((fetch () (shiftf (aref code pc) '(stop)))
-           (jmp (n) (incf pc n)))
-      (loop
-        (destructuring-bind (op . arg) (fetch)
-          (case op
-            (stop (return acc))
-            (nop (jmp 1))
-            (acc (incf acc arg) (jmp 1))
-            (jmp (jmp arg))))))))
+  (run (program in)))
+
+(defun part-2 (&optional (in 8))
+  (let ((code (program in)))
+    (dotimes (i (length code) :not-found)
+      (destructuring-bind (o . a) (aref code i)
+        (unless (eq o 'acc)
+          (let ((code (copy-seq code)))
+            (setf (aref code i) (cons (case o (jmp 'nop) (nop 'jmp)) a))
+            (multiple-value-bind (acc ok) (run code)
+              (when ok
+                (return acc)))))))))
 
 (define-test test
-  (assert (= 1930 (part-1))))
-
+  (assert (= 1930 (part-1)))
+  (assert (= 1688 (part-2))))
