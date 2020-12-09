72e2c25ff3c3bac69d883e16dd9d29e7fc26e56d
Christophe Junke <junke.christophe@gmail.com>
2020-12-09 08:25:44 +0100

    tests

--- days/d08.lisp
+++ days/d08.lisp
@@ -1,5 +1,7 @@
 (defpackage :aoc2020.08
   (:use :aoc2020)
+  (:shadowing-import-from
+   :sb-assem . #.(aoc2020:external-symbols :sb-assem))
   (:export #:test))
 
 (in-package :aoc2020.08)
@@ -22,6 +24,52 @@
         (values inst n))
       ;; normal end of program
       (values 'END :ok)))
+
+(import 'sb-vm::rax-tn)
+
+(let ((code #((nop . 3) (acc . 2) (jmp . -2))))
+  (let* ((asmstream (make-asmstream))
+         (*asmstream* asmstream)
+         (labels (map 'vector 
+                      (lambda (i) (gen-label (princ-to-string i)))
+                      code)))
+    ;; actual assembly
+    (assemble (:code 'nil)
+      (inst xor :dword rax-tn rax-tn)
+      (loop
+        for i below (length code)
+        for (op . n) across code
+        for lab = (aref labels i)
+        do (emit-label lab)
+           (inst mov
+                 :dword
+                 (sb-x86-64-asm::rip-relative-ea lab) #xc3)
+           (case op
+             (nop )
+             (acc (inst add :dword rax-tn n))
+             (jmp (inst jmp (aref labels (+ i n)))))))
+
+    ;; debugging
+    (let ((segment (assemble-sections asmstream nil (make-segment))))
+      (let ((vec (segment-contents-as-vector segment)))
+        (sb-sys:with-pinned-objects (vec) 
+          (sb-disassem:disassemble-memory (sb-sys:vector-sap vec)
+                                          (length vec)))))))
+
+
+(sb-sys:with-pinned-objects (vec) 
+  (sb-disassem:disassemble-memory (sb-sys:vector-sap vec) (length vec)))
+
+
+(defun assemble (code &aux (pc 0) (acc 0))
+
+  (loop
+    (multiple-value-bind (instruction n) (fetch code pc)
+      (ecase instruction
+        (END (return (values acc n)))
+        (NOP (incf pc 1))
+        (ACC (incf pc 1) (incf acc n))
+        (JMP (incf pc n))))))
 
 (defun run (code &aux (pc 0) (acc 0))
   (loop
@@ -50,4 +98,3 @@
 (define-test test
   (assert (= 1930 (part-1)))
   (assert (= 1688 (part-2))))
-
