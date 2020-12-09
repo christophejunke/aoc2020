225d5ba8fbef81e2d5869050c9a056d0ffa47e29
Christophe Junke <junke.christophe@gmail.com>
2020-12-09 09:57:24 +0100

    Remove multiple-value-call which prevents SERIES optimizations

--- days/d09.lisp
+++ days/d09.lisp
@@ -1,9 +1,10 @@
 (defpackage :aoc2020.09
-  (:use :aoc2020))
+  (:use :aoc2020)
+  (:export #:test))
 
 (in-package :aoc2020.09)
 
-(defun sum-p (value elements)
+(defun sum-p (value &rest elements)
   (block nil
     (map-combinations (lambda (v)
                         (destructuring-bind (a b) v
@@ -14,18 +15,18 @@
                       :length 2)
     value))
 
-(defmacro part-1/size (size stream)
-  `(z:collect-first
-    (z:choose
-      (multiple-value-call #'z:map-fn t
-        (lambda (&rest items)
-          (destructuring-bind (next . previous) (reverse items)
-            (sum-p next previous)))
-        (z:chunk ,(1+ size) 1 (z:scan-stream ,stream))))))
+(defmacro part-1-expand (size stream)
+  (let ((item (gensym "NEXT"))
+        (prev (loop repeat size collect (gensym "PREV"))))
+    `(z:collect-first
+      (z:choose
+       (z:mapping
+        (((,@prev ,item) (z:chunk ,(1+ size) 1 (z:scan-stream ,stream))))
+        (sum-p ,item ,@prev))))))
 
 (defun part-1 ()
   (with-input (s 9)
-    (part-1/size 26 s)))
+    (part-1-expand 26 s)))
 
 (defvar *part-1* 393911906)
 
@@ -33,4 +34,3 @@
   (with-input-from-string (in "35 20 15 25 47 40 62 55 65 95 102 117 150 182 127 219 299 277 309 576")
     (assert (= 127 (part-1/size 5 in))))
   (assert (= *part-1* (part-1))))
-
