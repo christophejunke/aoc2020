b8fcf35452f51a4131cf7a20bb88a5b5dd40bed4
Christophe Junke <junke.christophe@gmail.com>
2020-12-10 07:32:20 +0100

    Day 10 - part 1

--- days/d10.lisp
+++ days/d10.lisp
@@ -1,5 +1,24 @@
 (defpackage :aoc2020.10
-  (:use :aoc2020))
+  (:use :aoc2020)
+  (:export #:test))
 
 (in-package :aoc2020.10)
 
+(defun input ()
+  (sort (z:collect 'list (z:scan-file (fetch-input 10))) #'<))
+
+(defun part-1 (&aux (ones 0) (threes 1))
+  (loop
+    with ones = 0 and threes = 1
+    for last = 0 then jolt
+    for jolt in (input)
+    for diff = (- jolt last)
+    do (case diff
+         (1 (incf ones))
+         (2)
+         (3 (incf threes))
+         (t (error "unexpected")))
+    finally (return (* ones threes))))
+
+(define-test test ()
+  (assert (= 2176 (part-1))))
