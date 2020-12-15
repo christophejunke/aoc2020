20908ef87935bdf817a5f04e3faff746f91a0f32
Christophe Junke <junke.christophe@gmail.com>
2020-12-15 11:37:28 +0100

    Day 15 - part 1

--- days/d15.lisp
+++ days/d15.lisp
@@ -1,5 +1,51 @@
 (defpackage :aoc2020.15
-  (:use :aoc2020))
+  (:use :aoc2020)
+  (:export #:test))
 
 (in-package :aoc2020.15)
 
+(defvar *input* '(15 12 0 14 3 1))
+
+(defun make-env (list &optional (turn 1) result)
+  (if list
+      (make-env (rest list)
+                (1+ turn)
+                (acons (car list) turn result))
+      result))
+
+(defun value-turn (env value)
+  (cdr (assoc value env)))
+
+(defun last-spoken-value (env)
+  (destructuring-bind ((value . turn) . past-env) env
+    (values value
+            turn
+            (value-turn past-env value))))
+
+(defun speak (env turn value)
+  (values (acons value turn env)
+          turn))
+
+(defun play (env)
+  (multiple-value-bind (last-spoken turn past-turn) (last-spoken-value env)
+    (declare (ignore last-spoken))
+    (if past-turn
+        (speak env (1+ turn) (- turn past-turn))
+        (speak env (1+ turn) 0))))
+
+(defun play-until (list target-turn)
+  (let ((env (make-env list)) (turn 0))
+    (loop
+      (multiple-value-setq (env turn) (play env))
+      (when (>= turn target-turn)
+        (return env)))))
+
+(defun part-1 ()
+  (last-spoken-value (play-until *input* 2020)))
+
+(define-test test
+  (assert (equalp (play-until '(0 3 6) 10)
+                  '((0 . 10) (4 . 9) (0 . 8) (1 . 7) (3 . 6)
+                    (3 . 5) (0 . 4) (6 . 3) (3 . 2) (0 . 1))))
+  (assert (= 436 (last-spoken-value (play-until '(0 3 6) 2020))))
+  (assert (= 249 (part-1))))
