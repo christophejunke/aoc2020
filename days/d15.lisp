1b6b1c82ad137f341ab4fe2678c69c1cac06f588
Christophe Junke <junke.christophe@gmail.com>
2020-12-15 15:09:46 +0100

    remove last-spoken-value

--- days/d15.lisp
+++ days/d15.lisp
@@ -17,9 +17,6 @@
   (let ((env (make-env% list)))
     (values (alist-hash-table env) (cdar env))))
 
-(defmacro last-spoken-value (form)
-  `(nth-value 1 ,form))
-
 (defun play-until (list target-turn)
   (multiple-value-bind (env turn) (make-env list)
     (let (past-turn last-spoken)
@@ -29,20 +26,17 @@
           (shiftf past-turn (gethash value env) turn)
           (setq last-spoken value))
         (when (>= turn target-turn)
-          (return (values env last-spoken)))))))
+          (return last-spoken))))))
 
 (defun part-1 ()
-  (last-spoken-value (play-until *input* 2020)))
+  (play-until *input* 2020))
 
 (define-test test
-  (assert (equalp (play-until '(0 3 6) 10)
-                  '((0 . 10) (4 . 9) (0 . 8) (1 . 7) (3 . 6)
-                    (3 . 5) (0 . 4) (6 . 3) (3 . 2) (0 . 1))))
-  (assert (= 436 (last-spoken-value (play-until '(0 3 6) 2020))))
+  (assert (= 436 (play-until '(0 3 6) 2020)))
   (assert (= 249 (part-1))))
 
 (defun part-2 ()
-  (last-spoken-value (play-until *input* 30000000)))
+  (play-until *input* 30000000))
 
 (define-test test-part-2
   (assert (= 41687 (part-2))))
