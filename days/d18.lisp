5d9afb5de30dc386118d25c26f999b398867a673
Christophe Junke <junke.christophe@gmail.com>
2020-12-18 15:32:22 +0100

    better definition of group-by-priority, handle associativity

--- days/d18.lisp
+++ days/d18.lisp
@@ -1,6 +1,8 @@
 (defpackage :aoc2020.18
   (:use :aoc2020)
-  (:export #:test))
+  (:export #:test-solve
+           #:test-parsing
+           #:test-associativity))
 
 (in-package :aoc2020.18)
 
@@ -8,14 +10,17 @@
 
 ;; COMMON AST
 
+(defun intern-operator (op)
+  (intern op :aoc2020.18))
+
 (defrule int (+ (digit-char-p character))
   (:text t)
   (:function parse-integer))
 
-(defrule op (and #\space (or #\+ #\*) #\space)
+(defrule op (and #\space (or #\+ #\* #\^) #\space)
   (:function second)
   (:text t)
-  (:function intern))
+  (:function intern-operator))
 
 (defrule par (and #\( exp #\))
   (:function second))
@@ -30,7 +35,16 @@
 ;; EXP: a number or (A O1 B O2 C ...) where A B C are EXP
 (defrule exp atom*
   (:destructure (lhs &rest ops)
-    (list* lhs (mappend #'identity ops))))
+                (list* lhs (mappend #'identity ops))))
+
+(defmacro check-parsing (string ast)
+  `(assert (equalp (parse 'exp ,string) ',ast)))
+
+(define-test test-parsing
+  (check-parsing "5 + (8 * 3 + 9 + 3 * 4 * 3)"
+                 (5 + (8 * 3 + 9 + 3 * 4 * 3)))
+  (check-parsing "5 + 2 ^ 4"
+                 (5 + 2 ^ 4)))
 
 (defun group-by-priority (expr priority<)
   (flet ((recurse (e) (group-by-priority e priority<)))
@@ -40,8 +54,37 @@
       ((list a o b) (list (recurse a) o (recurse b)))
       ((list* a o1 b o2 rest)
        (if (funcall priority< o1 o2)
-           `(,(recurse a) ,o1 ,(recurse (list* b o2 rest)))
+           (recurse `(,a ,o1 (,b ,o2 ,@rest)))
            (recurse `((,a ,o1 ,b) ,o2 ,@rest)))))))
+
+(defun priority< (right-assoc-p priority)
+  (lambda (o1 o2)
+    (if (eq o1 o2)
+        (funcall right-assoc-p o1)
+        (< (funcall priority o1)
+           (funcall priority o2)))))
+
+(defun rank (x sequence)
+  (or (position-if (lambda (v)
+                     (typecase v
+                       (symbol (eq x v))
+                       (sequence (find x v))))
+                   sequence)
+      (error "~a not found in ~a" x sequence)))
+
+(defmacro check-group (< exp expected)
+  `(assert (equalp (group-by-priority (parse 'exp ,exp) ,<)
+                   ',expected)))
+
+(define-test test-associativity
+  (let ((p< (priority< (lambda (o) (member o '(^)))
+                       (lambda (o) (rank o '(* + ^))))))
+    (check-group p<
+                 "5 + (8 * 3 + 9 + 3 * 4 * 3)"
+                 (5 + (8 * ((((3 + 9) + 3) * 4) * 3))))
+    (check-group p<
+                 "3 + 4 + 5 + 2 ^ 5 ^ 6"
+                 (((3 + 4) + 5) + (2 ^ (5 ^ 6))))))
 
 (defun e (expr)
   (ematch expr
@@ -59,7 +102,7 @@
 (defun part-n (calc)
   (fold-input-lines 18 (lambda (line acc) (+ acc (funcall calc line))) 0))
 
-(define-test test
+(define-test test-solve
   (assert (= (calc-1 "2 * 3 + (4 * 5)") 26))
   (assert (= (calc-1 "5 + (8 * 3 + 9 + 3 * 4 * 3)") 437))
   (assert (= (calc-1 "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") 12240))
